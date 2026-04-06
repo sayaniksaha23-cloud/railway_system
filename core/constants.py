f5ab4900be66fc615584c4e601936ef1e27deeb4
Sayanik Saha <sayaniksaha23@gmail.com>
2026-04-07 00:09:08 +0530

    changed constants file

--- core/constants.py
+++ core/constants.py
@@ -1,50 +1,17 @@
 # constants.py to store the constants that have been used throughout the prograam
 
-# I wrote with spaces cuz Imma writing code in split screens
 TRAIN_TYPES = {
-    1: {
-        "name": "Superfast",
-        "speed": 120,
-        "premium": 1.5,
-        "surcharge": 100
-    },
-    2: {
-        "name": "Express",
-        "speed": 110,
-        "premium": 1.25,
-        "surcharge": 50
-    },
-    3: {
-        "name": "Fast Passenger",
-        "speed": 90,
-        "premium": 1.0,
-        "surcharge": 0
-    }
+    1: {"name": "Superfast", "speed": 120, "premium": 1.5, "surcharge": 100},
+    2: {"name": "Express", "speed": 110, "premium": 1.25, "surcharge": 50},
+    3: {"name": "Fast Passenger", "speed": 90, "premium": 1.0, "surcharge": 0}
 }
 CLASSES = {
-    1: {
-        "name": "Sleeper",
-        "multiplier": 1.0,
-        "baggage": 20
-    },
-    2: {
-        "name": "AC 3-Tier",
-        "multiplier": 1.5,
-        "baggage": 30
-    },
-    3: {
-        "name": "AC 2-Tier",
-        "multiplier": 2.0,
-        "baggage": 40
-    }
+    1: {"name": "Sleeper", "multiplier": 1.0, "baggage": 20},
+    2: {"name": "AC 3-Tier", "multiplier": 1.5, "baggage": 30},
+    3: {"name": "AC 2-Tier", "multiplier": 2.0, "baggage": 40}
 }
+
 PROMO_CODES = {
-    "ADG20": {
-        "type": "percentage",
-        "value": 20
-    },
-    "WINTER500": {
-        "type": "flat",
-        "value": 500
-    }
+    "ADG20": {"type": "percentage", "value": 20},
+    "WINTER500": {"type": "flat", "value": 500}
 }
